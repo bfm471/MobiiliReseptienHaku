87da72616efd7ce6402b4a490b390ed0bd05084d
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-09-09 20:55:49 +0300

    Toteutettu reseptien haku

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { ActivityIndicator, Button, FlatList, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [results, setResults] = useState([]);
+  const [loading, setLoading ] = useState(false);
+
+  const handleFind = () => {
+    setResults([]);
+    setLoading(true);
+    const url = 'https://www.themealdb.com/api/json/v1/1/filter.php';
+    fetch(`${url}?i=${keyword}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Virhe fetch-haussa:", response.status);
+      }
+      return response.json();
+    })
+    .then(data => setResults(data.meals))
+    .catch(e => console.error("Virhe:", e))
+    .finally(() => setLoading(false));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder='enter keyword' value={keyword} onChangeText={text => setKeyword(text)} />
+      <Button title='FIND' onPress={handleFind} />
+      {loading && <ActivityIndicator />}
+      <FlatList 
+        data={results}
+        ItemSeparatorComponent={<View style={styles.separator} />}
+        renderItem={({ item }) => (
+          <>
+            <Text style={styles.title}>{item.strMeal}</Text>
+            <Image 
+              style={styles.img}
+              source={{
+                uri: item.strMealThumb }} />
+          </>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +47,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  separator: {
+    height: 1,
+    backgroundColor: 'lightgray',
+    marginTop: 5,
+    marginBottom: 10,
+    width: '75%',
+    alignSelf: 'center'
+  },
+  title: {
+    fontSize: 20
+  },
+  img: {
+    width: 80,
+    height: 80
+  }
 });
